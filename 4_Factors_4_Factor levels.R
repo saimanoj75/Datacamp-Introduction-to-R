43e538fc062a93b40cf495456593c7c4a18f0771
saimanoj75 <36559913+saimanoj75@users.noreply.github.com>
2020-03-30 00:06:16 +0530

    Update 4_Factors_4_Factor levels.R

--- 4_Factors_4_Factor levels.R
+++ 4_Factors_4_Factor levels.R
@@ -3,6 +3,6 @@ survey_vector <- c("M", "F", "F", "M", "M")
 factor_survey_vector <- factor(survey_vector)
 
 # Specify the levels of factor_survey_vector
-levels(factor_survey_vector) <-
+levels(factor_survey_vector) <- c("Female", "Male")
 
 factor_survey_vector
